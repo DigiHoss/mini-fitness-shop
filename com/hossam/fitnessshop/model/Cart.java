8c3483dfe40f9d3bb8cbc07b83e1ca048eedbde9
Digihoss <hossamelhamdi6@gmail.com>
2026-09-06 00:09:32 +0200

    feat(model): implement Cart which holds, manages selected products and finalizes a purchase into an Order

---
+++ com/hossam/fitnessshop/model/Cart.java
@@ -0,0 +1,66 @@
+package com.hossam.fitnessshop.model;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cart {
+  private String owner;
+  private List<Product> products;
+
+  public Cart(String owner) {
+    this.owner = owner;
+    this.products = new ArrayList<Product>();
+  }
+
+  public void addProduct(Product product) {
+    this.products.add(product);
+  }
+
+  public boolean removeProduct(int id) {
+    Product productToRemove = null;
+    for (Product product : this.products) {
+      if (product.getId() == id) {
+        productToRemove = product;
+      }
+    }
+    if (productToRemove != null) {
+      return this.products.remove(productToRemove);
+    }
+    return false;
+  }
+
+  public String viewCartProducts() {
+    if (this.products.isEmpty()) {
+      return "Cart is empty !";
+    }
+    StringBuilder sb = new StringBuilder();
+    for (Product product : this.products) {
+      sb.append(
+          "id : "
+              + product.getId()
+              + " name : "
+              + product.getName()
+              + " price : "
+              + product.getPrice()
+              + "\n");
+    }
+    return sb.toString();
+  }
+
+  public double getTotalPrice() {
+    double totalPrice = 0;
+    for (Product product : this.products) {
+      totalPrice += product.getPrice();
+    }
+    return totalPrice;
+  }
+
+  public Order checkout() {
+    LocalDateTime localDateTime = LocalDateTime.now();
+    List<Product> confirmedProducts = new ArrayList<Product>(this.products);
+    Order order = new Order(this.owner, confirmedProducts, localDateTime, getTotalPrice());
+    this.products.clear();
+    return order;
+  }
+}
