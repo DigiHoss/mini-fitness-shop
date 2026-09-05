fdc0d53265fb2c96c3fadd7de396b6f29f411825
Digihoss <hossamelhamdi6@gmail.com>
2026-09-05 19:20:04 +0200

    feat(model): implement Catalog with CRUD, id generation and a repository sync

---
+++ com/hossam/fitnessshop/model/Catalog.java
@@ -0,0 +1,71 @@
+package com.hossam.fitnessshop.model;
+
+import java.util.Collections;
+import java.util.List;
+
+public class Catalog {
+  private List<Product> products;
+  private ProductRepository productRepository;
+
+  public Catalog(ProductRepository productRepository) {
+    this.products = productRepository.loadProducts();
+    this.productRepository = productRepository;
+  }
+
+  public List<Product> getAllProducts() {
+    return Collections.unmodifiableList(this.products);
+  }
+
+  public Product findProductById(int id) {
+    for (Product product : this.products) {
+      if (product.getId() == id) {
+        return product;
+      }
+    }
+    return null;
+  }
+
+  public int getNextId() {
+    if (this.products.isEmpty()) {
+      return 1;
+    }
+    int candidateId = 0;
+    for (Product product : this.products) {
+      if (product.getId() > candidateId) {
+        candidateId = product.getId();
+      }
+    }
+    return ++candidateId;
+  }
+
+  public void addProduct(String name, String description, double price) {
+    Product product = new Product(getNextId(), name, description, price);
+    this.products.add(product);
+    productRepository.saveProducts(this.products);
+  }
+
+  public boolean removeProduct(int id) {
+    Product product = findProductById(id);
+    if (product == null) {
+      return false;
+    } else {
+      boolean isRemoved = this.products.remove(product);
+      if (isRemoved) {
+        productRepository.saveProducts(this.products);
+      }
+      return isRemoved;
+    }
+  }
+
+  public boolean modifyProduct(int id, String newName, String newDescription, double newPrice) {
+    Product product = findProductById(id);
+    if (product == null) {
+      return false;
+    }
+    product.setName(newName);
+    product.setDescription(newDescription);
+    product.setPrice(newPrice);
+    productRepository.saveProducts(this.products);
+    return true;
+  }
+}
