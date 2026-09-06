49e08cc2a24318c66dac8e368d969694cca238ab
Digihoss <hossamelhamdi6@gmail.com>
2026-09-06 10:55:17 +0200

    feat(model): implement User which holds user data and compares candidate password against its own stored one

---
+++ com/hossam/fitnessshop/model/User.java
@@ -0,0 +1,29 @@
+package com.hossam.fitnessshop.model;
+
+public abstract class User {
+  private String fullName;
+  private String username;
+  private String password;
+
+  public User(String fullName, String username, String password) {
+    this.fullName = fullName;
+    this.username = username;
+    this.password = password;
+  }
+
+  public String getFullName() {
+    return this.fullName;
+  }
+
+  public String getUsername() {
+    return this.username;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public boolean isPasswordMatch(String candidatePassword) {
+    return this.password.equals(candidatePassword);
+  }
+}
