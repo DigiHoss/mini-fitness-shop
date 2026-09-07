a9817755dd06abadb57fa4c3988fc387142ccb3a
Digihoss <hossamelhamdi6@gmail.com>
2026-09-07 11:23:19 +0200

    feat(model): implement Customer's interactive session menu

--- com/hossam/fitnessshop/model/Customer.java
+++ com/hossam/fitnessshop/model/Customer.java
@@ -1,7 +1,10 @@
 package com.hossam.fitnessshop.model;
 
+import com.hossam.fitnessshop.repository.UserRepository;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Customer extends User {
   private Cart cart;
@@ -41,8 +44,117 @@ public class Customer extends User {
   }
 
   @Override
-  public void runSession() {
-    //
+  public void runSession(Scanner sc, Catalog catalog, UserRepository userRepository) {
+    // System.out.println("customer session active");
+    int choice = 0;
+    do {
+      System.out.println("login in as a Customer");
+      System.out.println("Full name: " + super.getFullName());
+      System.out.println("-----------------------------------");
+      System.out.println("Menu :");
+      System.out.println("1. View catalog products");
+      System.out.println("2. View my actual cart");
+      System.out.println("3. Confirm your selected products");
+      System.out.println("4. Log out");
+      System.out.println("-----------------------------------");
+      System.out.print("Your choice : ");
+      String input = sc.nextLine();
+      choice = Integer.parseInt(input);
+      switch (choice) {
+        case 1: {
+          System.out.println("-----------------------------------");
+          System.out.println("Available products :");
+          System.out.println(viewCatalogProducts(catalog));
+          System.out.println("-----------------------------------");
+          System.out.println("-----------------------------------");
+          System.out.println("Menu :");
+          System.out.println("1. Add a product to my cart");
+          System.out.println("2. Go back");
+          System.out.println("-----------------------------------");
+          System.out.print("Your choice : ");
+          String innerInput = sc.nextLine();
+          int innerChoice = Integer.parseInt(innerInput);
+          switch (innerChoice) {
+            case 1:
+              System.out.print("To add product to your cart, Enter his Id: ");
+              String inputId = sc.nextLine();
+              int id = Integer.parseInt(inputId);
+              Product selectedProduct = catalog.findProductById(id);
+              if (selectedProduct == null) {
+                System.out.println("Product with id : " + id + " does not exist !");
+              } else {
+                this.cart.addProduct(selectedProduct);
+                System.out.println("Product with id : " + id + " Added Successfully");
+              }
+              break;
+            case 2:
+              System.out.println("-----------------------------------");
+              System.out.println("Menu :");
+              System.out.println("1. View catalog products");
+              System.out.println("2. View my actual cart");
+              System.out.println("3. Confirm your selected products");
+              System.out.println("4. Log out");
+              System.out.println("-----------------------------------");
+              break;
+              default:
+                System.out.println("Please enter a number from the available choices !");
+          }
+        }; break;
+        case 2: {
+          System.out.println("-----------------------------------");
+          System.out.println("Cart's products :");
+          System.out.println(this.cart.viewCartProducts());
+          System.out.println("Total to pay : " + this.cart.getTotalPrice());
+          System.out.println("-----------------------------------");
+          System.out.println("-----------------------------------");
+          System.out.println("Additional Menu :");
+          System.out.println("1. Remove a product from my cart");
+          System.out.println("2. Go back");
+          System.out.println("-----------------------------------");
+          System.out.print("Your choice : ");
+          String innerInput = sc.nextLine();
+          int innerChoice = Integer.parseInt(innerInput);
+          switch (innerChoice) {
+            case 1:
+              System.out.print("To remove a product from your cart, Enter his Id: ");
+              String inputId = sc.nextLine();
+              int id = Integer.parseInt(inputId);
+              this.cart.removeProduct(id);
+              break;
+            case 2:
+              System.out.println("-----------------------------------");
+              System.out.println("Menu :");
+              System.out.println("1. View catalog products");
+              System.out.println("2. View my actual cart");
+              System.out.println("3. Confirm your selected products");
+              System.out.println("4. Log out");
+              System.out.println("-----------------------------------");
+              break;
+            default:
+              System.out.println("Please enter a number from the available choices !");
+          }
+        }; break;
+        case 3: {
+          System.out.println("To confirm your products to checkout, please enter \"YES\"");
+          System.out.print("Your enter : ");
+          String innerInput = sc.nextLine();
+          if (innerInput.trim().toUpperCase().equals("YES")) {
+            System.out.println("Passed");
+            System.out.println(checkout());
+          } else {
+            System.out.println("Refused ! please reselect again the choice 3 and follow the instructions !");
+          }
+        }; break;
+        case 4:
+          System.out.println("Logging out ... ! see you the next time.");
+          return;
+
+          default:
+            System.out.println("Please enter a number from the available choices !");
+            break;
+      }
+
+    } while (choice != 4);
   }
 
   @Override
